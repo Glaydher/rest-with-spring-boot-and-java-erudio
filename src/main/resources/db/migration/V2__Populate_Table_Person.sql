b6b09a90f2cb403b3e5a4ccbc1f0c50f98141a3a
heydher feijó <glaydher@gmail.com>
2023-01-13 00:42:42 -0300

    Serializando em YML

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` VALUES 
+	(1,'Ayrton','Senna','São Paulo','Male'),
+	(2,'Leonardo','Da Vinci','Italia','Male'),
+	(3,'Indira ','Gandhi','India','Male'),
+	(4,'Muhammad','Ali','Kentucky - US','Male'),
+	(5,'Nelson ','Mandela','Mvezo - South Africa','Male'),
+	(6,'Nikola','Tesla','Smiljan - Croácia','Male');
